781e6b4a0d7ed405cfb297d8e90580b81a7819ef
OleMarGlo <om.glomsrud@gmail.com>
2026-09-14 15:11:18 +0200

    feat: sticky connections

--- src/redis_clone/server.clj
+++ src/redis_clone/server.clj
@@ -1,12 +1,32 @@
-(ns redis-clone.server)
+(ns redis-clone.server
+  (:require [redis-clone.command :as commands])
+  (:import [java.net ServerSocket]
+           [java.io BufferedReader InputStreamReader PrintWriter]))
+
+(defn handle-client
+  [client]
+  (let [reader (-> client
+                   (.getInputStream)
+                   (InputStreamReader.)
+                   (BufferedReader.))
+        writer (PrintWriter.
+                (.getOutputStream client)
+                true)]
+    (loop []
+      (let [request (.readLine reader)]
+        (when request
+          (println "Recieved:" request)
+
+          (let [response (commands/handle-command request)]
+            (.println writer response))))
+      (recur))))
 
 (defn start!
   [port]  
-  (with-open [server (java.net.ServerSocket. port)]
+  (with-open [server (ServerSocket. port)]
     (println (str "Starting server on localhost:" port))
 
     (let [client (.accept server)]
       (println "Client connected:" (.getRemoteSocketAddress client))
-      (.close client))))
-
+      (handle-client client))))
 
